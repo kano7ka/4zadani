f4058b6169186c4591b5f0423c126b49de803329
Izabella Maksimenko <115064225+kano7ka@users.noreply.github.com>
2023-04-02 12:31:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 def vienkarss_get():
-  response = requests.get(url)
+  response = requests.get('https://api.chucknorris.io/')
   if response.status_code == 200:
     print("Web site exists")
   else:
